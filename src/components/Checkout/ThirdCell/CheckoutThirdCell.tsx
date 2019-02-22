8f9b1b644f5d67b6a40671378dca040485e626db
Harvey Chui <chuiharvey@gmail.com>
2019-02-22 13:09:14 -0800

    Finished with the basic parts

--- src/components/Checkout/ThirdCell/CheckoutThirdCell.tsx
+++ src/components/Checkout/ThirdCell/CheckoutThirdCell.tsx
@@ -1,15 +1,26 @@
 import React, { SFC } from "react";
 import { Text } from "react-native";
 
-import styles from "../common/styles";
+import styles from "./styles";
 import i18n from "../../../../i18n";
 import CheckoutCells from "../common/CheckoutCells";
+import TwoColumnRow from "../../common/TwoColumnRow";
+import BoldText from "../../common/BoldText";
 
-const CheckoutThirdCell: SFC = props => {
-  const { checkoutTitle } = styles
+interface IProps {
+  dollarSymbol: string;
+  totalPrice: string;
+}
+
+const CheckoutThirdCell: SFC<IProps> = props => {
+  const { dollarSymbol, totalPrice } = props
+  const { checkoutPrice, checkoutPriceDesc } = styles
   return (
     <CheckoutCells>
-      <Text style={checkoutTitle}>{i18n.t("Checkout.checkout")}</Text>
+      <TwoColumnRow 
+        left= {<BoldText style={checkoutPriceDesc}>{i18n.t("Checkout.total")}</BoldText>}
+        right={<BoldText style={checkoutPrice}>{`${dollarSymbol} ${totalPrice}`}</BoldText>}
+      />
     </CheckoutCells>
     
   );
---
+++ src/components/Checkout/ThirdCell/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  checkoutPrice: {
+    fontSize: 23,
+  },
+  checkoutPriceDesc: {
+    fontSize: 21
+  }
+});
+
+export default styles;
