f8598d9d6583b80032bccbc4954160ff21b7b316
Harvey Chui <chuiharvey@gmail.com>
2019-02-21 17:44:14 -0800

    added some simple file structure to the project and initalized the Checkout files in the components and containers

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './src';
+import { appSettings } from './src/settings';
 
-AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.registerComponent(appSettings.appName, () => App);
---
+++ src/containers/Checkout/Checkout.tsx
@@ -0,0 +1,17 @@
+/**
+ * This is where the actual logic for checkout lives
+ */
+
+import React, { Component } from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+type Props = {
+  
+};
+
+export default class Checkout extends Component<Props> {
+  render() {
+    return (
+    );
+  }
+}
