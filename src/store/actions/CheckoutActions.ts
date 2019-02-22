cdd12cb23a8482836ac4b93ec91c2a117ec8370b
Harvey Chui <chuiharvey@gmail.com>
2019-02-21 18:19:41 -0800

    Finished the CheckoutActions

---
+++ src/store/actions/CheckoutActions.ts
@@ -0,0 +1,14 @@
+import { checkoutActionTypes } from "./actionTypes";
+
+const {
+  APPLY_COUPON_CODE
+} = checkoutActionTypes;
+
+const applyCouponCodeAction = (couponCode: string) => ({
+  type: APPLY_COUPON_CODE,
+  payload: couponCode
+})
+
+export const applyCouponCode = (couponCode: string) => (dispatch: any) => {
+  dispatch(applyCouponCodeAction(couponCode));
+}
