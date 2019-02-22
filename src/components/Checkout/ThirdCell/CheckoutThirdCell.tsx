c10f63b4ab7efb7c3f97edaa2038476fe154b1cf
Harvey Chui <chuiharvey@gmail.com>
2019-02-22 12:09:26 -0800

    Added the files for the second to fourth checkout cells

---
+++ src/components/Checkout/ThirdCell/CheckoutThirdCell.tsx
@@ -0,0 +1,18 @@
+import React, { SFC } from "react";
+import { Text } from "react-native";
+
+import styles from "../common/styles";
+import i18n from "../../../../i18n";
+import CheckoutCells from "../common/CheckoutCells";
+
+const CheckoutThirdCell: SFC = props => {
+  const { checkoutTitle } = styles
+  return (
+    <CheckoutCells>
+      <Text style={checkoutTitle}>{i18n.t("Checkout.checkout")}</Text>
+    </CheckoutCells>
+    
+  );
+};
+
+export default CheckoutThirdCell;
