c4e550363c7458df0031f08dbf399bc03db70860
Harvey Chui <chuiharvey@gmail.com>
2019-02-22 02:57:34 -0800

    Changed react-native-languages to react-native-localize, seems like they made the change in the last couple months

--- i18n/i18n.ts
+++ i18n/i18n.ts
@@ -1,9 +1,9 @@
-import RNLanguages from "react-native-languages";
+import * as RNLocalize from "react-native-localize";
 import i18n from "i18n-js";
 
 import en from "./locales/en.json";
 
-i18n.locale = RNLanguages.language;
+i18n.locale = RNLocalize.getCountry();
 i18n.fallbacks = true;
 i18n.translations = { en };
 
